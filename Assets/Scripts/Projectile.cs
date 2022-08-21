3db799e14bf9ff4b2d035119e18f364579907e8d
Mikhail <fentoru@gmail.com>
2022-08-21 06:14:39 +0500

    v1.63 let projectiles do damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,4 +15,25 @@ public class Projectile : MonoBehaviour {
 	void Update () {
 		transform.Translate(Vector3.right * speed * Time.deltaTime);
 	}
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		GameObject obj = collider.gameObject;
+		if (obj.GetComponent<Attacker>() && obj.GetComponent<Health>()){
+			Health targetHealth = obj.GetComponent<Health>();
+			targetHealth.GetDamage(damage);
+			Destroy(gameObject);
+		}
+		
+	}
+	
+	// способ рабочий, но стоит ли так делать ?
+	/* void OnTriggerEnter2D(Collider2D collider){
+		Attacker attacker = collider.GetComponent<Attacker>();
+		Health targetHealth = collider.GetComponent<Health>();
+		if (attacker && targetHealth){
+			targetHealth.GetDamage(damage);
+			Destroy(gameObject);
+		}
+	}
+	*/
 }
