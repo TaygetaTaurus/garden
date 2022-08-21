117e83fb5266e6e4d9bbcfe9e511e3037dec9179
Mikhail <fentoru@gmail.com>
2022-08-21 10:23:52 +0500

    v1.65 do projectiles parent

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,12 +4,23 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
 
 	public GameObject projectile;
-	public GameObject projectileParent;
 	public GameObject gun;
+	
+	private GameObject projectileParent;
+	
+	void Start(){
+		projectileParent = GameObject.Find("Projectiles");
+		
+		if(!projectileParent){
+			projectileParent = new GameObject("Projectiles");
+		}
+	}
 	
 	private void Fire(){
 		GameObject newProjectile = Instantiate(projectile) as GameObject;
+		
 		newProjectile.transform.parent = projectileParent.transform;
 		newProjectile.transform.position = gun.transform.position;
 	}
+	
 }
