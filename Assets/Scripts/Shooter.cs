372177e18736330926f7d6fcd1300a8c2beb054e
Mikhail <fentoru@gmail.com>
2022-08-22 07:44:37 +0500

    v1.68 shooters detect attackers now

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,14 +6,57 @@ public class Shooter : MonoBehaviour {
 	public GameObject projectile;
 	public GameObject gun;
 	
+	private Spawner myLaneSpawner;
+	private Animator animator;
 	private GameObject projectileParent;
 	
 	void Start(){
-		projectileParent = GameObject.Find("Projectiles");
+		animator = GetComponent<Animator>();
 		
+		//creates parrent if necessary
+		projectileParent = GameObject.Find("Projectiles");
 		if(!projectileParent){
 			projectileParent = new GameObject("Projectiles");
 		}
+		
+		SetMyLaneSpawner();
+	}
+	
+	void Update(){
+		if(IsAttackerAheadInLane()){
+			animator.SetBool("isAttacking", true);
+		}else{
+			animator.SetBool("isAttacking", false);
+		}
+	}
+	
+	void SetMyLaneSpawner(){
+		Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner>();
+		
+		foreach(Spawner thisSpawner in spawnerArray){
+			if (thisSpawner.transform.position.y == transform.position.y){
+				myLaneSpawner = thisSpawner;
+				return;
+			}	
+		}
+		
+		Debug.LogError(name + " Cant find spawner for y = " + transform.position.y + " lane");
+	}
+	
+	bool IsAttackerAheadInLane(){
+		//exit if no attackers in lane
+		if(myLaneSpawner.transform.childCount <= 0){
+			return false;
+		}
+		
+		//if there are attackers, are they ahead?
+		foreach(Transform attacker in myLaneSpawner.transform){
+			if(attacker.transform.position.x > transform.position.x){
+				return true;
+			}
+		}
+		//Attacker in lane, but behind us
+		return false;
 	}
 	
 	private void Fire(){
